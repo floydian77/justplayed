906e5cf8d20dcc98da25be4d0fc1914aa94f451e
floydian77 <pieterh42+github@gmail.com>
2018-03-10 00:32:56 +0100

    Bootstrap layout

---
+++ resources/views/collection/index.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="starter-template">
+        <h1>Collection</h1>
+        <p class="lead">
+            Collection ...
+        </p>
+    </div>
+@stop
---
+++ resources/views/settings/index.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="starter-template">
+        <h1>Settings</h1>
+        <p class="lead">
+            Settings ...
+        </p>
+    </div>
+@stop
