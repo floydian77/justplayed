df2ccb774b9da2cbb8b5f7627c3c41871a282263
floydian77 <pieterh42+github@gmail.com>
2018-03-15 20:45:17 +0100

    discogs:fetch-releases

--- app/Console/Commands/Discogs/FetchReleases.php
+++ app/Console/Commands/Discogs/FetchReleases.php
@@ -2,16 +2,17 @@
 
 namespace App\Console\Commands\Discogs;
 
-use Illuminate\Console\Command;
 
-class FetchReleases extends Command
+use Illuminate\Support\Facades\Redis;
+
+class FetchReleases extends DiscogsCommand
 {
     /**
      * The name and signature of the console command.
      *
      * @var string
      */
-    protected $signature = 'discogs:fetch-releases';
+    protected $signature = 'discogs:fetch-releases {id=1}';
 
     /**
      * The console command description.
@@ -37,6 +38,47 @@ class FetchReleases extends Command
      */
     public function handle()
     {
-        //
+        parent::handle();
+
+        $this->info("Fetching all releases");
+
+        $ids = $this->fetchIds();
+        $total = count($ids);
+        foreach($ids as $key => $id) {
+            $this->line(sprintf(
+                "Fetching release:%d (%d / %d)",
+                $id, $key+1, $total
+            ));
+            $this->call('discogs:fetch-release', [
+                'id' => $id
+            ]);
+        }
+
+    }
+
+    /**
+     * Get ids of releases that not have been fetched yet.
+     *
+     * @return array
+     */
+    private function fetchIds()
+    {
+        // Get ids of all releases in user collection.
+        $userCollectionHashName = "user:$this->id:discogs:collection";
+        $collection = Redis::hgetall($userCollectionHashName);
+        $collectionIds = array();
+        foreach($collection as $item) {
+            $release = json_decode($item);
+            array_push($collectionIds, $release->id);
+        }
+
+        // Get ids of all fetched releases
+        $releasesHashName = "discogs:releases";
+        $releaseIds = Redis::hkeys($releasesHashName);
+
+        // Get ids of releases to fetch
+        $fetchIds = array_diff($collectionIds, $releaseIds);
+
+        return $fetchIds;
     }
 }
